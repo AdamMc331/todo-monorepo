1070baf1410ef0839ac47963264fa40834906e48
adammc331 <amcneilly331@gmail.com>
2018-10-05 22:59:22 -0400

    Migrated to AndroidX.

--- todo-mvp/src/main/java/com/adammcneilly/todo/addtask/AddTaskActivity.kt
+++ todo-mvp/src/main/java/com/adammcneilly/todo/addtask/AddTaskActivity.kt
@@ -2,7 +2,7 @@ package com.adammcneilly.todo.addtask
 
 import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.adammcneilly.todo.R
--- todo-mvp/src/main/java/com/adammcneilly/todo/tasklist/TaskListActivity.kt
+++ todo-mvp/src/main/java/com/adammcneilly/todo/tasklist/TaskListActivity.kt
@@ -3,8 +3,8 @@ package com.adammcneilly.todo.tasklist
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.adammcneilly.todo.R
 import com.adammcneilly.todo.Task
 import com.adammcneilly.todo.addtask.AddTaskActivity
